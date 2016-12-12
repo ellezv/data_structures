14eb22d8a8195cf147685e5e7ab0268345b7cdd7
Maelle Vance <maellevance@gmail.com>
2016-12-12 13:27:59 -0800

    setup for our linked_list module

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""The setup for our data_structures project."""
+
+from setuptools import setup
+
+setup(
+    name="Data structures",
+    description="Implementations of various data structures in Python",
+    version=0.1,
+    author="Maelle Vance, Sera Smith",
+    author_email="maellevance@gmail.com, seras37@gmail.ocom",
+    license="MIT",
+    py_modules=[
+        'linked_list'
+    ],
+    package_dir={'': 'src'},
+    install_requires=[],
+    extras_require={
+        "test": ["tox", "pytest", "pytest-watch", "pytest-cov"]
+    },
+)
---
+++ src/linked_list.py
@@ -0,0 +1,1 @@
+"""Python implementation of a linked list."""
