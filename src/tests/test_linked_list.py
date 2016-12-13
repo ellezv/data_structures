3cd2b0336fe7a6f8c2516c10ed52220326ee290b
Your Name <you@example.com>
2016-12-12 23:40:15 -0800

    tests recovered

---
+++ src/tests/test_linked_list.py
@@ -0,0 +1,34 @@
+"""Tests for our linked_list module."""
+
+import pytest
+
+
+def test_linked_list():
+    """Test the instantiation of our Linked List."""
+    from linked_list import LinkedList
+    lst = LinkedList()
+    if lst.head is None:
+        pass
+
+
+def test_node():
+    """Test the instantiation of a node."""
+    from linked_list import Node
+    new_node = Node(1, 2)
+    assert new_node.value == 1 and new_node.next == 2
+
+
+def test_push_linked_list():
+    """Test the push method in our LinkedList class."""
+    from linked_list import LinkedList
+    lst = LinkedList()
+    lst.push(2)
+    assert lst.head.value == 2
+
+
+def test_pop_linked_list():
+    """Test pop method in our LinkedList class."""
+    from linked_list import LinkedList
+    lst = LinkedList()
+    with pytest.raises(IndexError):
+        lst.pop()
