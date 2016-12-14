43606b3e298cf441178a5692379ecbaf12ad1a09
Your Name <you@example.com>
2016-12-14 14:39:19 -0800

    append method written. switching drivers for test.

--- src/dll.py
+++ src/dll.py
@@ -5,6 +5,7 @@ class Node():
     """Instantiate a node."""
 
     def __init__(self, value=None, nxt=None, previous=None):
+        """."""
         self.value = value
         self.next = nxt
         self.previous = previous
@@ -14,18 +15,29 @@ class DbLinkedList():
     """Instantiate a doubly linked list."""
 
     def __init__(self, value=None):
+        """."""
         self.head = None
         self.tail = None
         if value:
             self.push(value)
 
     def push(self, value=None):
-        """Pushes vale to the head of dll."""
+        """Push value to the head of dll."""
         new_node = Node(value, self.head, None)
         orig_node = self.head
         self.head = new_node
         new_node.next = orig_node
-        new_node.prev_node = None
         if self.tail is None:
-            self.tail = orig_node
+            self.tail = self.head
 
+    def append(self, value):
+        """Append value to the tail of dll."""
+        new_node = Node(value, None, self.tail)
+        orig_node = self.tail
+        self.tail = new_node
+        new_node.prev_node = orig_node
+        if self.head is None:
+            self.head = self.tail
+
+    def pop(self):
+        """Pop first value off of the head of dll."""
