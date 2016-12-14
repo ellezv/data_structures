62dd41605ea8c40481ba47efb5ab7b3a4408e323
Maelle Vance <maellevance@gmail.com>
2016-12-13 21:11:47 -0800

    tweeked stack init function and wrote more tests. 100% cov and all pass

--- src/stack.py
+++ src/stack.py
@@ -1,22 +1,18 @@
 """Implementation of a stack in Python, inheriting from a linked list."""
-from linked_list import LinkedList, Node
+from linked_list import LinkedList
 
 
 class Stack(object):
     """Create stack which inherits from LinkedList class."""
 
-    def __init__(self, head=None, iterable=None):
+    def __init__(self, iterable=None):
         """Create a new stack, from LinkedList using composition."""
         self._linkedlist = LinkedList()
-        if iterable:
-            try:
-                for item in iterable:
-                    self._linkedlist.head = Node(item)
-                    self._linkedlist.length += 1
-            except TypeError:
-                self._linkedlist.head = iterable
-        else:
-            self._linkedlist.head = head
+        if iterable and hasattr(iterable, "__iter__"):
+            for item in iterable:
+                self.push(item)
+        elif iterable:
+            raise TypeError
 
     def push(self, value):
         """Push a new value on top of the stack."""
--- src/tests/test_stack.py
+++ src/tests/test_stack.py
@@ -1,1 +1,42 @@
 """Tests for our stack module."""
+
+import pytest
+
+
+@pytest.fixture
+def new_stack():
+    from stack import Stack
+    stck = Stack()
+    return stck
+
+
+def test_new_stack_is_empty(new_stack):
+    """Test that instantiation of stack is empty."""
+    assert new_stack._linkedlist.head is None
+
+
+def test_push_stack(new_stack):
+    """Test that push stack pushes value on top of stack."""
+    new_stack.push(5)
+    assert new_stack._linkedlist.head.value is 5
+
+
+def test_pop_stack(new_stack):
+    """Test that pop method pops value off top of stack."""
+    new_stack.push(5)
+    new_stack.pop()
+    assert new_stack._linkedlist.head is None
+
+
+def test_new_stack_head_is_last_iter():
+    """Test that the new_stack head is the last iterable"""
+    from stack import Stack
+    stck = Stack([1,2,3,4,5])
+    assert stck._linkedlist.head.value is 5
+
+
+def test_new_stack_without_iter_raises_typeerr():
+    """Test that instantiating a new stack with a non iter raises Type Error."""
+    from stack import Stack
+    with pytest.raises(TypeError):
+        Stack(42)
