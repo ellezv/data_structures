56fb66c6554becbc9bd9c93281c93a0fb1651c48
Your Name <you@example.com>
2016-12-15 16:34:59 -0800

    wrote test_init x 2

--- src/tests/test_queue.py
+++ src/tests/test_queue.py
@@ -1,1 +1,29 @@
 """Test for our queue module."""
+
+import pytest
+
+@pytest.fixture
+def new_queue():
+    """Initialize new queue."""
+    from queue import Queue
+    queue = Queue(5)
+    queue.enqueue(4)
+    queue.enqueue(3)
+    return queue
+
+def test_init_queue():
+    """Test a initiation of queue."""
+    from queue import Queue
+    new_queue = Queue()
+    assert new_queue.length == 0
+    assert new_queue.head == None
+    assert new_queue.tail == None
+
+def test_queue_with_value():
+    """General test to initiate queue with value."""
+    from queue import Queue
+    new_queue = Queue(5)
+    assert new_queue.head.value == 5
+    assert new_queue.tail.value == 5
+    assert new_queue.length == 1
+
