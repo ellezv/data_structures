8e2e386c2fdb1cb748511e7142b7e6f66431ae54
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2016-12-19 14:58:01 -0800

    commiting tests for append, appendleft, and pop.

--- src/tests/test_deque.py
+++ src/tests/test_deque.py
@@ -32,3 +32,44 @@ def test_init_deque_with_iter_sets_tail(init_deque):
 def test_init_deque_with_iter_sets_length(init_deque):
     """Check length is increased properly."""
     assert init_deque._dblinkedlist.length == 5
+
+
+def test_append_one_value(init_deque):
+	"""Check the appended value is added to the end of the deque."""
+	init_deque.append(0)
+	assert init_deque._dblinkedlist.tail.value = 0
+
+
+def test_length_append_one_value(init_deque):
+	"""Check deque length after a value is appended to the end deque."""
+	init_deque.append(0)
+	assert init_deque._dblinkedlist.length = 6
+
+
+def test_appendleft_one_value(init_deque):
+	"""Check the appended value is added to the front of the deque."""
+	init_deque.appendleft(6)
+	assert init_deque._dblinkedlist.head.value = 6
+
+
+def test_length_appendleft_one_value(init_deque):
+	"""Check deque length after a value is appended to the front deque."""
+	init_deque.appendleft(6)
+	assert init_deque._dblinkedlist.length = 6
+
+
+def test_end_pop_one_value(init_deque):
+	""""""
+	init_deque.pop()
+	assert init_deque._dblinkedlist.tail.value = 2
+
+
+def test_length_pop_one_value(init_deque):
+	""""""
+	init_deque.pop()
+	assert init_deque._dblinkedlist.length = 4
+
+
+def test_value_pop_one_value(init_deque):
+	""""""
+	assert init_deque.pop() = 1
