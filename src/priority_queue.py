60fd443647b40b0e3a76d4c264c4b1c028e0bcd2
Maelle Vance <maellevance@gmail.com>
2016-12-21 14:52:19 -0800

    init function pass test. Need to refactor to take list of tuples instead of one tuple.

--- src/priority_queue.py
+++ src/priority_queue.py
@@ -1,5 +1,17 @@
+"""An implementation of a priority queue using a dictionary in Python."""
 
 
-class Priority_Queue(object):
+class PriorityQueue(object):
+    """That's it."""
 
-    
+    def __init__(self, tup=None):
+        """Initialize a priority queue with default value as None."""
+        self._container = {}
+        if tup and type(tup) == tuple:
+            self.insert(tup)
+        elif tup:
+            raise TypeError("Must initialize with tuple!")
+
+    def insert(self, tup):
+        """Insert tuple in priority queue."""
+        pass
