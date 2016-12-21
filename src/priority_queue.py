8d7cbfa6671a0dabc84039df145e654980326598
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2016-12-21 15:49:28 -0800

    commiting implementatino of priority queue, added pop and peek, and try excepts and raises.

--- src/priority_queue.py
+++ src/priority_queue.py
@@ -4,14 +4,31 @@
 class PriorityQueue(object):
     """That's it."""
 
-    def __init__(self, tup=None):
+    def __init__(self):
         """Initialize a priority queue with default value as None."""
         self._container = {}
-        if tup and type(tup) == tuple:
-            self.insert(tup)
-        elif tup:
-            raise TypeError("Must initialize with tuple!")
 
-    def insert(self, tup):
+    def insert(self, value, priority=0):
         """Insert tuple in priority queue."""
-        pass
+        if priority > 0:
+            raise ValueError('Priorities can only be negative, David.')
+        self._container.setdefault(priority, []).append(value)
+
+    def pop(self):
+        """"""
+        try:
+            highest_priority = min([priority for priority in self._container])
+            highest_priority_value = self._container[highest_priority].pop(0)
+            if not len(self._container[highest_priority]):
+                self._container.pop(highest_priority)
+            return highest_priority_value
+        except ValueError:
+            raise IndexError('Cannot pop from an empty priority queue!')
+
+    def peek(self):
+        """"""
+        try:
+            highest_priority = min([priority for priority in self._container])
+            return self._container[highest_priority][0]
+        except ValueError:
+            raise IndexError('There is nothing to see there, David.')
