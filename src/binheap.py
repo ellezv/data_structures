8223d8de34cd0063cf8d9655789f08e18cdcc854
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2016-12-21 03:21:54 -0800

    commiting push, pop, and tests. refactored for an organize_binheap function. need to commit more.

--- src/binheap.py
+++ src/binheap.py
@@ -3,7 +3,14 @@ import math
 
 
 class Binheap(object):
-    """."""
+    """A binary heap (max heap).
+
+    The binary heap pushes the largest values up
+    to the top of the heap. Values must be numbers.
+
+    push(val) Pushes a value into the binary heap.
+    pop() Pops a value off the top of the binary heap.
+    """
 
     def __init__(self, value=None):
         """Initialize binary heap."""
@@ -14,12 +21,31 @@ class Binheap(object):
     def push(self, value):
         """Push a value in our heapified list."""
         self._container.append(value)
+        if len(self._container) > 1:
+            self.organize_binheap()
 
+    def pop(self):
+        """Pop a value from the top of the heap."""
+        try:
+            self._container[0], self._container[len(self._container) - 1] = self._container[len(self._container) - 1], self._container[0]
+            self._container.pop()
+            if len(self._container) > 1:
+                self.organize_binheap()
+        except:
+            raise IndexError('Cannot pop from an empty binary heap!')
+
+    def organize_binheap(self):
+        """Organize the binary heap according to the heaps rules.
+
+        Max heap: Parent nodes must be greater than child nodes.
+
+        Notes:  Last index number is (container length -1).
+                Parent index is (container length - 2) / 2 floored.
+        """
         a = len(self._container) - 1
-        b = math.floor((len(self._container) - 1) / 2)
+        b = int(math.floor((len(self._container) - 2) / 2))
         while self._container[a] > self._container[b]:
-            try:
-                self._container[a], self._container[b] = self._container[b], self._container[a]
-                a, b = b, math.floor(b / 2)
-            except IndexError:
+            self._container[a], self._container[b] = self._container[b], self._container[a]
+            a, b = b, int(math.floor((b - 1) / 2))
+            if self._container[a] is self._container[0]:
                 break
--- src/tests/test_binheap.py
+++ src/tests/test_binheap.py
@@ -1,14 +1,77 @@
 """Tests for our binheap module."""
 
-def test_init_empty_node():
-    from binheap import Node
-    new_node = Node()
-    assert new_node.value is None
-    assert new_node.right is None
-    assert new_node.left is None
+
+import pytest
 
 
-def test_init_empty_binheap():
+def test_init_empty():
+    """Test binary heap empty on init"""
     from binheap import Binheap
     new_binheap = Binheap()
-    assert new_binheap.top is None
+    assert new_binheap._container == []
+
+
+def test_init_binheap():
+    """Tests initialization of binary heap"""
+    from binheap import Binheap
+    new_binheap = Binheap(1)
+    assert new_binheap._container == [1]
+
+
+def test_binheap_push_single_value_container_head():
+    """Tests push on binary heap"""
+    from binheap import Binheap
+    new_binheap = Binheap()
+    new_binheap.push(1)
+    assert new_binheap._container == [1]
+
+
+def test_binheap_push_two_values_second_greater_than():
+    """Tests push on binary heap"""
+    from binheap import Binheap
+    new_binheap = Binheap()
+    new_binheap.push(1)
+    new_binheap.push(2)
+    assert new_binheap._container == [2, 1]
+
+
+def test_binheap_push_two_values_second_less_than():
+    """Tests push on binary heap"""
+    from binheap import Binheap
+    new_binheap = Binheap()
+    new_binheap.push(2)
+    new_binheap.push(1)
+    assert new_binheap._container == [2, 1]
+
+
+def test_binheap_push_multiple_unordered():
+    """Tests push on binary heap"""
+    from binheap import Binheap
+    new_binheap = Binheap()
+    new_binheap.push(5)
+    new_binheap.push(4)
+    new_binheap.push(6)
+    new_binheap.push(3)
+    new_binheap.push(7)
+    new_binheap.push(2)
+    new_binheap.push(9)
+    assert new_binheap._container == [9, 6, 7, 3, 4, 2, 5]
+
+
+def test_binheap_push_one_value_pop_one_value():
+    """Tests push on binary heap"""
+    from binheap import Binheap
+    new_binheap = Binheap()
+    new_binheap.push(2)
+    new_binheap.pop()
+    assert new_binheap._container == []
+
+
+def test_binheap_push_two_values_pop_one_value():
+    """Tests push on binary heap"""
+    from binheap import Binheap
+    new_binheap = Binheap()
+    new_binheap.push(5)
+    new_binheap.push(4)
+    new_binheap.pop()
+    assert new_binheap._container == [4]
