1518f47d066fed532b7b9e6cf069f769cf397e29
Maelle Vance <maellevance@gmail.com>
2017-01-01 14:38:31 -0800

    initial commit: new simple graph module, new test module for simple graph updated setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,18 @@ setup(
     name="Data structures",
     description="Implementations of various data structures in Python",
     version=0.1,
-    author="Maelle Vance, Sera Smith",
-    author_email="maellevance@gmail.com, seras37@gmail.ocom",
+    author="Maelle Vance, Sera Smith, Ben Shields",
+    author_email="maellevance@gmail.com, seras37@gmail.com",
     license="MIT",
     py_modules=[
-        'linked_list', 'stack', 'dll', 'queue', 'deque', 'priority_queue'
+        'linked_list',
+        'stack',
+        'dll',
+        'queue',
+        'deque',
+        'priority_queue',
+        'binheap',
+        'simple_graph'
     ],
     package_dir={'': 'src'},
     install_requires=[],
---
+++ src/tests/test_simple_graph.py
@@ -0,0 +1,1 @@
+"""Test for our simple graph module."""
