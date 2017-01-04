e5c62e9658a898a62b81ad23dec0cf775457f97a
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2017-01-04 11:05:35 -0800

    reverted to original neighbors function so that neigbhors returns list of nodes and weights.

--- src/weighted_graph.py
+++ src/weighted_graph.py
@@ -86,10 +86,7 @@ class Graph(object):
     def neighbors(self, node):
         """Return list of edges of the node given."""
         if node in self._nodes.keys():
-            return_me = []
-            for each in self._nodes[node]:
-                return_me.append(each[0])
-            return return_me
+            return self._nodes[node]
         raise ValueError("Node is not in graph.")
 
     def adjacent(self, n1, n2):
