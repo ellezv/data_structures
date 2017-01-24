3bd280d295c27927083a0ba190100403ed8f4c00
JoeyDeRosa <joeyderosa11.jd@gmail.com>
2017-01-23 18:31:02 -0800

    hash table complete with tests

--- src/hash_table.py
+++ src/hash_table.py
@@ -4,23 +4,42 @@
 class HashTable(object):
     """A hash table."""
 
-    def __init__(self, size=10):
+    def __init__(self, size=10, hash_type='additive'):
         """Init the hash table."""
         if type(size) is not int or size < 1:
             raise TypeError('Size must be a positive integer greater than zero.')
         self._size = size
         self._table = [[] for i in range(size)]
+        if hash_type is not 'additive' and hash_type is not 'bernstein':
+            raise TypeError('The second argument of hash type must be either additive or bernstein.')
+        self._hash_type = hash_type
 
     def get(self, key):
         """Return the value paired with the key."""
         bucket = self._hash(key)
+        for pair in self._table[bucket]:
+            if pair[0] == key:
+                return pair[1]
 
     def set(self, key, value):
         """Create a key value pair for the given key and value."""
         if type(key) is not str:
             return 'Key must be a string.'
-        self._table[self._hash(key)].append(value)
+        self._table[self._hash(key)].append((key, value))
 
     def _hash(self, key):
         """Create a hash for the given key."""
+        if self._hash_type is 'additive':
+            return self._additive(key)
+        return self._bernstein(key)
+
+    def _additive(self, key):
+        """An additive hash."""
         return sum([ord(i) for i in key]) % self._size
+
+    def _bernstein(self, key):
+        """A bernstein hash."""
+        the_hash = 0
+        for i in key:
+            the_hash = 33 * the_hash + ord(i)
+        return the_hash % self._size
--- src/tests/test_hash_table.py
+++ src/tests/test_hash_table.py
@@ -1,6 +1,12 @@
 """Test file for hash table."""
 
+import io
 import pytest
+
+
+file = io.open("/etc/dictionaries-common/words")
+word_file = file.read()
+WORDS = word_file.split("\n")
 
 
 @pytest.fixture
@@ -15,12 +21,22 @@ def empty_ht():
 def pop_ht():
     """A populated hash table."""
     from hash_table import HashTable
-    ht = HashTable(5)
-    ht.set('apple', 0)
-    ht.set('three', 1)
-    ht.set('vw', 2)
-    ht.set('zero', 3)
-    ht.set('cake', 4)
+    ht = HashTable(5, 'bernstein')
+    ht.set('d', 0)
+    ht.set('e', 1)
+    ht.set('a', 2)
+    ht.set('b', 3)
+    ht.set('c', 4)
+    return ht
+
+
+@pytest.fixture
+def get_test_ht():
+    """A hash table filled from /usr/share/dict/words."""
+    from hash_table import HashTable
+    ht = HashTable(10000)
+    for i in WORDS:
+        ht.set(i, i)
     return ht
 
 
@@ -49,21 +65,44 @@ def test_init_non_int_given():
         HashTable('yo')
 
 
-def test_hash(empty_ht):
-    """Test the hash method returns the apropriate table index."""
-    assert empty_ht._hash('apple') == 530 % empty_ht._size
+def test_init_no_hash_type(empty_ht):
+    """Set the hash type to additive if none is given."""
+    assert empty_ht._hash_type == 'additive'
+
+
+def test_init_with_hash_type(pop_ht):
+    """Set hash type to the given type."""
+    assert pop_ht._hash_type == 'bernstein'
+
+
+def test_init_with_bad_hash_type():
+    """Test that init throws a value error if something other than additive and bernstein is given."""
+    from hash_table import HashTable
+    message = 'The second argument of hash type must be either additive or bernstein.'
+    with pytest.raises(TypeError, message=message):
+        HashTable(2, 'Manitoba')
+
+
+def test_additive(empty_ht):
+    """Test the hash method returns the appropriate table index."""
+    assert empty_ht._additive('apple') == 530 % empty_ht._size
+
+
+def test_bernstein(empty_ht):
+    """Test that bernstein returns the appropriate table index."""
+    assert empty_ht._bernstein('apple') == 119184914 % empty_ht._size
 
 
 def test_set_empty_bucket(empty_ht):
-    """Test that set puts the value in the apropriate bucket."""
+    """Test that set puts the value in the appropriate bucket."""
     empty_ht.set('a', 1)
-    assert empty_ht._table[17] == [1]
+    assert empty_ht._table[17] == [('a', 1)]
 
 
 def test_set_populated_bucket(pop_ht):
     """Test that set puts a value in the next index of a populated bucket."""
     pop_ht.set('seattle', 'second index')
-    assert pop_ht._table[4] == [4, 'second index']
+    assert pop_ht._table[1] == [('e', 1), ('seattle', 'second index')]
 
 
 def test_set_non_string_key_given(pop_ht):
@@ -73,4 +112,26 @@ def test_set_non_string_key_given(pop_ht):
 
 def test_get_single_item_in_bucket(pop_ht):
     """Test that get retrives the value paired with the given key."""
-    assert pop_ht.get('vw') == 2
+    assert pop_ht.get('a') == 2
+
+
+def test_get_multiple_items_in_bucket(pop_ht):
+    """Test that get retrives the value paired with the given key."""
+    pop_ht.set('seattle', 1234567890)
+    assert pop_ht.get('seattle') == 1234567890
+
+
+def test_hash_additive(empty_ht):
+    """Test that hash returns additive hash when additive is the hash type."""
+    assert empty_ht._hash('yo') == 12
+
+
+def test_hash_bernstein(pop_ht):
+    """Test that hash returns bernstein hash when bernstein is the hash type."""
+    assert pop_ht._hash('yo') == 4
+
+
+@pytest.mark.parametrize('word', WORDS)
+def ultimate_get_test(get_test_ht, word):
+    """Test the get method for a giant hash table of words."""
+    assert get_test_ht(word) == word
