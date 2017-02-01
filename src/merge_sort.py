1d753497f1ef75bb097b2b9094b08e3bbea6dbc6
Maelle Vance <maellevance@gmail.com>
2017-02-01 09:15:18 -0800

    added if name==main block

--- src/merge_sort.py
+++ src/merge_sort.py
@@ -24,3 +24,21 @@ def merge_sort(input_list):
     else:
         sorted_list += back_half
     return sorted_list
+
+
+if __name__ == "__main__":
+    import timeit
+
+    easy_sort_time = timeit.repeat(stmt="merge_sort([2,3,1])", setup="from merge_sort import merge_sort", number=500, repeat=1)
+    complex_sort_time = timeit.repeat(stmt="merge_sort([i for i in range(1000)][::-1])", setup="from merge_sort import merge_sort", number=500, repeat=1)
+    output = """merge sort is a simple sorting algorithm that builds the final sorted array (or list) one item at a time.
+
+    Input: [2,3,1]
+    number of runs: 500
+    average time: {0} seconds
+
+    Input: [i for i in range(1000)]
+    number of runs: 500
+    average time: {1} seconds""".format(easy_sort_time, complex_sort_time)
+
+    print(output)
