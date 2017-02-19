a5104151e12903960b41e87fdb9da535a73a4afa
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2017-02-19 02:23:27 -0800

    cleaned pop function.

--- src/deque.py
+++ src/deque.py
@@ -42,14 +42,14 @@ class Deque(object):
 
     def peek(self):
         """Return the value of the end of the deque without removing it."""
-        if self._dblinkedlist.head is None:
-            return self._dblinkedlist.head
+        if self._dblinkedlist.tail is None:
+            return
         return self._dblinkedlist.tail.value
 
     def peekleft(self):
         """Return the value of the front of the deque without removing it."""
         if self._dblinkedlist.head is None:
-            return self._dblinkedlist.head
+            return
         return self._dblinkedlist.head.value
 
     def size(self):
